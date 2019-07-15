22701b7133a971e5e96d0da1180fdac3082fde16
z5060508 <you.zhu@unsw.edu.au>
2019-07-15 14:43:39 +1000

    Created test folder for TestPlayerMovement

---
+++ src/test/test/test/TestPlayerMovement.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package test.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Admin
+ *
+ */
+class TestPlayerMovement {
+
+	/**
+	 * Test method for {@link unsw.dungeon.Player#Player(unsw.dungeon.Dungeon, int, int)}.
+	 */
+	@Test
+	void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link unsw.dungeon.Player#moveUp()}.
+	 */
+	@Test
+	void testMoveUp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link unsw.dungeon.Player#moveDown()}.
+	 */
+	@Test
+	void testMoveDown() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link unsw.dungeon.Player#moveLeft()}.
+	 */
+	@Test
+	void testMoveLeft() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link unsw.dungeon.Player#moveRight()}.
+	 */
+	@Test
+	void testMoveRight() {
+		fail("Not yet implemented");
+	}
+
+}
