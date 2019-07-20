01333d25f0ca77a866abcc3497db1649995cc643
z5060508 <you.zhu@unsw.edu.au>
2019-07-20 17:43:07 +1000

    door test written

--- src/unsw/test/KeyDoorTest.java
+++ src/unsw/test/KeyDoorTest.java
@@ -1,81 +1,39 @@
 package unsw.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Test;
 import unsw.dungeon.*;
-import unsw.playerState.InvincibleState;
 public class KeyDoorTest {
 	
 	@Test
-	public void testMoveLeft() {
-		Dungeon maze = new Dungeon(15, 15);
-		Player c1 = new Player(maze, 1, 1);
-		Enemy c2 = new Enemy(3,1);
-		maze.addEntity(c1);
-		maze.setPlayer(c1);
-		maze.addEntity(c2);
-		c1.registerObservers();
-		
-		c1.moveLeft(maze);
-		assertEquals(2,c2.getX());
-	}
-	
-	@Test
-	public void testMoveRight() {
-		Dungeon maze = new Dungeon(15, 15);
-		Player c1 = new Player(maze, 5, 1);
-		Enemy c2 = new Enemy(3,1);
-		maze.addEntity(c1);
-		maze.setPlayer(c1);
-		maze.addEntity(c2);
-		c1.registerObservers();
-		
-		c1.moveRight(maze);
-		assertEquals(4,c2.getX());
-	}
-	
-	@Test
-	public void testMoveUp() {
-		Dungeon maze = new Dungeon(15, 15);
-		Player c1 = new Player(maze, 2, 1);
-		Enemy c2 = new Enemy(3,6);
-		maze.addEntity(c1);
-		maze.setPlayer(c1);
-		maze.addEntity(c2);
-		c1.registerObservers();
-		
-		c1.moveRight(maze);
-		assertEquals(5,c2.getY());
-	}
-	
-	@Test
-	public void testMoveDown() {
-		Dungeon maze = new Dungeon(15, 15);
-		Player c1 = new Player(maze, 2, 6);
-		Enemy c2 = new Enemy(3,1);
-		maze.addEntity(c1);
-		maze.setPlayer(c1);
-		maze.addEntity(c2);
-		c1.registerObservers();
-		
-		c1.moveRight(maze);
-		assertEquals(2,c2.getY());
-	}
-	
-	
-	//  0 1 2 3 4
-	// 0  _ e _ _ _
-	// 1  _ w _ _ _
-	// 2  ->p
-	// 3
-	// 4
-	@Test
-	public void testMovementCheckingWall() {
+	public void testKeyDoorExist() {
 		Dungeon maze = new Dungeon(5, 5);
-		Player c1 = new Player(maze, 1, 2);
-		Enemy c2 = new Enemy(2,0);
-		Wall c3 = new Wall(2,1);
+		Player c1 = new Player(maze, 1, 1);
+		Key c2 = new Key(2,1);
+		Door c3 = new Door(3,1);
+		maze.addEntity(c1);
+		maze.setPlayer(c1);
+		maze.addEntity(c2);
+		maze.addEntity(c3);
+		c1.registerObservers();
+		
+		List<Entity> box = maze.findEntity(2, 1);
+		assertTrue(box.contains(c2));
+		box = maze.findEntity(3, 1);
+		assertTrue(box.contains(c3));
+	}
+	
+	
+	@Test
+	public void testKeyPickup() {
+		Dungeon maze = new Dungeon(5, 5);
+		Player c1 = new Player(maze, 1, 1);
+		Key c2 = new Key(2,1);
+		Door c3 = new Door(3,1);
 		maze.addEntity(c1);
 		maze.setPlayer(c1);
 		maze.addEntity(c2);
@@ -83,63 +41,38 @@ public class KeyDoorTest {
 		c1.registerObservers();
 		
 		c1.moveRight(maze);
-		assertEquals(0,c2.getY());
+		assertTrue(!c1.getKeys().isEmpty());
 	}
 	
-	//  0 1 2 3 4 5
-	// 0  _ _ _ _ _
-	// 1  _ e b _ p
-	// 2
-	// 3
-	// 4
 	@Test
-	public void testMovementPushingBoulder() {
-		Dungeon maze = new Dungeon(6, 6);
-		Player c1 = new Player(maze, 5, 0);
-		Enemy c2 = new Enemy(2,1);
-		Boulder c3 = new Boulder(3,1);
+	public void testKeyUnlock() {
+		Dungeon maze = new Dungeon(5, 5);
+		Player c1 = new Player(maze, 1, 1);
+		Key c2 = new Key(2,1);
+		Door c3 = new Door(3,1);
 		maze.addEntity(c1);
 		maze.setPlayer(c1);
 		maze.addEntity(c2);
 		maze.addEntity(c3);
 		c1.registerObservers();
 		
-		c1.moveDown(maze);
-		assertEquals(2,c2.getX());
-	}
-	
-	@Test
-	public void testInteractSword() {
-		Dungeon maze = new Dungeon(7, 7);
-		Player c1 = new Player(maze, 5, 0);
-		Enemy c2 = new Enemy(2,1);
-		Sword c3 = new Sword(3,1);
-		maze.addEntity(c1);
-		maze.setPlayer(c1);
-		maze.addEntity(c2);
-		maze.addEntity(c3);
-		c1.registerObservers();
+		// Check locked
+		assertTrue(!c3.isOpen());
+		// Check door id matches key id
+		assertEquals(c2.getKeyID(), c3.getKeyId());
+		//pickup key
+		boolean checkMove = c1.moveRight(maze);
+		assertTrue(checkMove);
 		
-		c1.moveDown(maze);
-		c1.moveRight(maze);
-		assertEquals(4,c2.getX());
-		assertEquals(3,c3.getX());
-	}
-	
-	@Test
-	public void testMoveDownInvincible() {
-		Dungeon maze = new Dungeon(15, 15);
-		Player c1 = new Player(maze, 2, 6);
-		Enemy c2 = new Enemy(3,1);
-		maze.addEntity(c1);
-		maze.setPlayer(c1);
-		maze.addEntity(c2);
-		c1.registerObservers();
-		c1.setPlayerState(new InvincibleState());
+		//move onto door and unlock
+		checkMove = c1.moveRight(maze);
+		assertTrue(checkMove);
+		assertTrue(c3.isOpen());
 		
+		// move through door
 		c1.moveRight(maze);
-		assertEquals(0,c2.getY());
+		assertEquals(4, c1.getX());
+		assertTrue(c1.getKeys().isEmpty());
 	}
-	
 	
 }
